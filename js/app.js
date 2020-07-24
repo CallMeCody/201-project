bc1c3fbc6f49fa4a16e4a8ea8eecaf83636103df
Cody Carpenter <cody_carpenter2222@yahoo.com>
2020-07-23 20:41:24 -0700

    added functions for questions

--- js/app.js
+++ js/app.js
@@ -1,40 +1,106 @@
 'use strict';
 
-var name = prompt('what is your name?');
-console.log('User\'s name is', name);
-alert('Hello! Welcome, ' + name + '.');
+var nameinput;
 
-var answer1 = prompt('Am I 25 years old?');
-if (answer1 === true)
-{
-  console.log('Correct!');
-  // alert('Correct!');
+askName();
+askQuestion1();
+askQuestion2();
+askQuestion3();
+askQuestion4();
+askQuestion5();
+
+
+function askName() {
+  nameinput = prompt('What is your name?');
+  // console.log('Hello! ' + nameinput + '. My name is Cody. I\'m going to ask five questions to see what you know about me.');
+  alert('Hello! ' + nameinput + '. My name is Cody. I\'m going to ask five questions to see what you know about me.');
 }
 
-var answer2 = confirm('Do I have 1 kid?');
-if (answer2 === false)
-{
-  console.log('Correct!');
-  // alert('Correct!');
+// Question 1
+
+
+function askQuestion1() {
+  var answer1 = prompt('Am I 25 years old?', 'Type yes or no.').toLowerCase();
+
+  while (answer1 !== 'yes' && answer1 !== 'no' && answer1 !== 'y' && answer1 !== 'n') {
+    answer1 = prompt('Please answer yes or no.');
+  }
+  if (answer1 === 'yes' || answer1 === 'y') {
+    // console.log('Correct!');
+    alert('Correct!');
+  }
+  else if (answer1 === 'no' || answer1 === 'n') {
+    // console.log('Incorrect!');
+    alert('Incorrect!');
+  }
 }
 
-var answer3 = confirm('Am I married?');
-if (answer3 === true)
-{
-  console.log('Correct!');
-  // alert('Correct!');
+// Question 2
+
+function askQuestion2() {
+  var answer2 = prompt('Am I married?', 'Type yes or no.').toLowerCase();
+  while (answer2 !== 'yes' && answer2 !== 'no' && answer2 !== 'y' && answer2 !== 'n') {
+    answer2 = prompt('Please answer yes or no.');
+  }
+  if (answer2 === 'yes' || answer2 === 'y') {
+    // console.log('Correct!');
+    alert('Correct!');
+  }
+  else if (answer2 === 'no' || answer2 === 'n') {
+    // console.log('Incorrect!');
+    alert('Incorrect!');
+  }
 }
 
-var answer4 = confirm('Did I grow up in Crescent City, CA?');
-if (answer4 === true)
-{
-  console.log('Correct!');
-  // alert('Correct!');
+// Question 3
+
+
+function askQuestion3() {
+  var answer3 = prompt('Do I have one kid?', 'Type yes or no.');
+  while (answer3 !== 'yes' && answer3 !== 'no' && answer3 !== 'y' && answer3 !== 'n') {
+    answer3 = prompt('Please answer yes or no.');
+  }
+  if (answer3 === 'yes' || answer3 === 'y') {
+    // console.log('Incorrect!');
+    alert('Incorrect');
+  }
+  else if (answer3 === 'no' || answer3 === 'n') {
+    // console.log('Correct!');
+    alert('Correct!');
+  }
 }
 
-var answer5 = confirm('Was I in the Army for 10 years?');
-if (answer5 === false)
-{
-  console.log('Correct!');
-  // alert('Correct!');
+// Question 4
+
+
+function askQuestion4() {
+  var answer4 = prompt('Did I grow up in Crescent City, CA?');
+  while (answer4 !== 'yes' && answer4 !== 'no' && answer4 !== 'y' && answer4 !== 'n') {
+    answer4 = prompt('Please answer yes or no.');
+  }
+  if (answer4 === 'yes' || answer4 === 'y') {
+    // console.log('Correct!');
+    alert('Correct!');
+  }
+  else if (answer4 === 'no' || answer4 === 'n') {
+    // console.log('Incorrect!');
+    alert('Incorrect!');
+  }
+}
+
+// Question 5
+
+function askQuestion5() {
+  var answer5 = prompt('Was I in the army for 10 years?');
+  while (answer5 !== 'yes' && answer5 !== 'no' && answer5 !== 'y' && answer5 !== 'n') {
+    answer5 = prompt('Please answer yes or no.');
+  }
+  if (answer5 === 'yes' || answer5 === 'y') {
+    // console.log('Incorrect! Thanks for playing ' + nameinput + '.');
+    alert('Incorrect! Thanks for playing ' + nameinput + '.');
+  }
+  else if (answer5 === 'no' || answer5 === 'n') {
+    // console.log('Correct! Thanks for playing ' + nameinput + '.');
+    alert('Correct! Thanks for playing ' + nameinput + '.');
+  }
 }
